9ae3d0ec1cc36fa09a86fdea6fd4fe59531e79a7
Nikhil jain <nikhiljain28806@gmail.com>
2023-12-05 23:11:47 +0530

    dded 1716. Calculate Money in Leetcode Bank

---
+++ 1716. Calculate Money in Leetcode Bank.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int week = n/7;
+        int rem = n%7;
+        return ((7*8)/2) * week + 7*week*(week-1)/2 + (rem*(rem+1)/2) + rem*week;
+    }
+};
