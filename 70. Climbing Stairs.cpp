fdd285a787404d3958263e5a7fc145487b409bbd
Nikhil jain <nikhiljain28806@gmail.com>
2024-01-18 09:16:35 +0530

    added 70. Climbing Stairs

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev = 1;
+        int curr = 1;
+        for(int i=2;i<=n;i++){
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+};
