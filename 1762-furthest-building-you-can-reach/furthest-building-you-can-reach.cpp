2834faf6d7066cb67796e994631508e4a5fcdaf1
Nikhil jain <nikhiljain28806@gmail.com>
2024-02-17 10:35:56 +0530

    Time: 128 ms (15.27%) | Memory: 72 MB (7.09%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -3,32 +3,30 @@ public:
 
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         vector<int> diff;
-        vector<int> indx;
         int n = heights.size();
-        for(int i=1;i<n;i++){
-            if(heights[i]-heights[i-1]>0){
-                diff.push_back(heights[i]-heights[i-1]);
-                indx.push_back(i);
-            }
-        }
         int ans = 0;
         multiset<int> mx;
         int brick_used = 0;
         bool flag = true;
-        for(int i=0;i<diff.size();i++){
+        for(int i=1;i<n;i++){
+            if(heights[i]-heights[i-1]<=0){
+                continue;
+            }
+            int diff = heights[i]-heights[i-1];
             if(mx.size()<ladders){
-                mx.insert(diff[i]);
+                mx.insert(diff);
             }
             else {
-                mx.insert(diff[i]);
+                mx.insert(diff);
                 brick_used += *mx.begin();
                 mx.erase(mx.begin());
                 if(brick_used>bricks){
                     flag = false;
-                    ans = indx[i]-1;
+                    ans = i-1;
                     break;
                 }
             }
+            
         }
         if(flag) return n-1;
         return ans;
