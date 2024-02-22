cc9b88aed77e952466200f61fe840731ee7dc5ef
Nikhil jain <nikhiljain28806@gmail.com>
2024-02-22 08:38:57 +0530

    Time: 219 ms (10.02%) | Memory: 101.3 MB (5.08%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -4,18 +4,13 @@ public:
         int pep = trust.size();
         if(n==1 && pep==0) return 1;
         map<int, set<int>> mp;
-        set<int> present;
-        for(int i=1;i<=n;i++){
-            present.insert(i);
-        }
+        vector<int> present(n+1,0);
         for(auto t: trust){
             mp[t[1]].insert(t[0]);
-            if(present.find(t[0])!=present.end()){
-                present.erase(t[0]);
-            }
+            present[t[0]] = 1;
         }
         for(auto pr: mp){
-            if(pr.second.size()==n-1 && present.find(pr.first)!=present.end()){
+            if(pr.second.size()==n-1 && !present[pr.first]){
                 return pr.first;
             }
         }
