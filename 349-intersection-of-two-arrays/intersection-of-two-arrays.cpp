3794fa155da6a29bce30ce2d5633fdbdb3ca1b12
Nikhil jain <nikhiljain28806@gmail.com>
2024-03-10 12:50:59 +0530

    Time: 15 ms (7.23%) | Memory: 12.3 MB (73.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,6 +3,13 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
+
+        nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
+        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end());
+        for(auto it: nums1) cout<<it<<" ";
+        cout<<endl;
+        for(auto it: nums2) cout<<it<<" ";
+        cout<<endl;
         int i = 0,j=0;
         int n1 = nums1.size();
         int n2 = nums2.size();
@@ -11,13 +18,12 @@ public:
             if(nums1[i] == nums2[j]){
                 ans.push_back(nums1[i]);
                 i++;j++;
-                while(i>0 && i<n1 && nums1[i]==nums1[i-1]){
-                    i++;
-                }
-                while(j>0 && j<n2 && nums2[j]==nums2[j-1]){
-                    j++;
-                }
-                if(i>=n1 || j>=n2) break;
+                // while(i>0 && i<n1 && nums1[i]==nums1[i-1]){
+                //     i++;
+                // }
+                // while(j>0 && j<n2 && nums2[j]==nums2[j-1]){
+                //     j++;
+                // }
             }
             else if(nums1[i]>nums2[j]){
                 j++;
