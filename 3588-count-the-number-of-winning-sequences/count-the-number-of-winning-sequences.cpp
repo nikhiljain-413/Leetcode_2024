715193d821739ff4b2a9b84e2e03b1d2f69de2de
Nikhil jain <nikhiljain28806@gmail.com>
2024-10-13 16:07:13 +0530

    Time: 1586 ms (16.67%) | Memory: 513.3 MB (8.33%) - LeetSync

--- 3588-count-the-number-of-winning-sequences/count-the-number-of-winning-sequences.cpp
+++ 3588-count-the-number-of-winning-sequences/count-the-number-of-winning-sequences.cpp
@@ -2,28 +2,28 @@
 
 class Solution {
 public:
-    map<char,ll> mp;
-    ll MOD = 1e9+7;
-    int dp[1001][2002][4];
-    ll solve(string &s, ll indx, ll last, ll cnt){
+    map<char,int> mp;
+    int MOD = 1e9+7;
+    int n;
+    ll solve(string &s, int indx, int last, int cnt, vector<vector<vector<int>>> &dp){
         if(indx == s.size()){
-            return cnt>1000?1:0;
+            return cnt>n?1:0;
         }
         if(dp[indx][cnt][last]!=-1) return dp[indx][cnt][last];
-        ll curr = mp[s[indx]];
+        int curr = mp[s[indx]];
         ll ans = 0;
         if(curr == 0){
-            if(last!=0) ans += solve(s, indx+1, 0, cnt)%MOD;
-            if(last!=1) ans += solve(s, indx+1, 1, cnt+1)%MOD;
-            if(last!=2) ans += solve(s, indx+1, 2, cnt-1)%MOD;
+            if(last!=0) ans += solve(s, indx+1, 0, cnt, dp)%MOD;
+            if(last!=1) ans += solve(s, indx+1, 1, cnt+1, dp)%MOD;
+            if(last!=2) ans += solve(s, indx+1, 2, cnt-1, dp)%MOD;
         }else if(curr == 1){
-            if(last!=0) ans += solve(s, indx+1, 0, cnt-1)%MOD;
-            if(last!=1) ans += solve(s, indx+1, 1, cnt)%MOD;
-            if(last!=2) ans += solve(s, indx+1, 2, cnt+1)%MOD;
+            if(last!=0) ans += solve(s, indx+1, 0, cnt-1, dp)%MOD;
+            if(last!=1) ans += solve(s, indx+1, 1, cnt, dp)%MOD;
+            if(last!=2) ans += solve(s, indx+1, 2, cnt+1, dp)%MOD;
         }else{
-            if(last!=0) ans += solve(s, indx+1, 0, cnt+1)%MOD;
-            if(last!=1) ans += solve(s, indx+1, 1, cnt-1)%MOD;
-            if(last!=2) ans += solve(s, indx+1, 2, cnt)%MOD;
+            if(last!=0) ans += solve(s, indx+1, 0, cnt+1, dp)%MOD;
+            if(last!=1) ans += solve(s, indx+1, 1, cnt-1, dp)%MOD;
+            if(last!=2) ans += solve(s, indx+1, 2, cnt, dp)%MOD;
         }
         return dp[indx][cnt][last] = ans%MOD;
     }
@@ -31,10 +31,12 @@ public:
         // f e f 0 2 0
         // w f w 1 0 1
         // e w e 2 1 2
-        memset(dp, -1, sizeof(dp));
+        n = s.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2*n,vector<int> (4,-1)));
+        // memset(dp, -1, sizeof(dp));
         mp['F'] = 0;
         mp['W'] = 1;
         mp['E'] = 2;
-        return solve(s, 0, 3,1000);
+        return solve(s, 0, 3,n, dp);
     }
 };
