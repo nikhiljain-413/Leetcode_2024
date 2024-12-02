ec0094bfb677cfcf9dec94a40fbef2304c54745c
Nikhil jain <nikhiljain28806@gmail.com>
2024-12-03 00:23:24 +0530

    Time: 4 ms (5.43%) | Memory: 8.8 MB (6.42%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,5 +1,25 @@
+struct Node{
+    Node* arr[26];
+    int flag = -1;
+    void setFlag(int indx){
+        flag = flag==-1?indx:flag;
+    }
+    int getFlag(){
+        return flag;
+    }
+    bool hasNext(char ch){
+        return arr[ch-'a']!=NULL;
+    }
+    Node* next(char ch){
+        return arr[ch-'a'];
+    }
+    void put(char ch){
+        arr[ch-'a'] = new Node();
+    }
+};
 class Solution {
 public:
+    Node* start = new Node();
     int isPrefixOfWord(string sentence, string searchWord) {
         string s;
         stringstream ss(sentence);
@@ -7,11 +27,30 @@ public:
         while(getline(ss, s ,' ')){
             words.push_back(s);
         }
-        int i = 1;
+        int indx = 1;
+        Node* trav;
         for(auto word: words){
-            if(word.size()>=searchWord.size() && word.substr(0,searchWord.size()) == searchWord) return i;
-            i++;
+            trav = start;
+            cout<<indx<<" "<<word<<endl;
+            for(auto ch: word){
+                cout<<ch<<endl;
+                if(!trav->hasNext(ch)){
+                    trav->put(ch);
+                }
+                trav = trav->next(ch);
+                trav->setFlag(indx);
+            }
+            // if(trav->getFlag()==-1) trav->setFlag(indx);
+            indx++;
         }
-        return -1;
+        trav = start;
+        for(auto ch: searchWord){
+            if(trav->hasNext(ch)){
+                trav = trav->next(ch);
+            }else{
+                return -1;
+            }
+        }
+        return trav->getFlag();
     }
 };
