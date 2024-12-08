e6ff2e595db0a0e2625c019034e6f9fade54dca7
Nikhil jain <nikhiljain28806@gmail.com>
2024-12-08 14:47:51 +0530

    Time: 50 ms (91.89%) | Memory: 131 MB (66.23%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
+    using info = tuple<int, bool, int>;
     int maxTwoEvents(vector<vector<int>>& events) {
         int n = events.size();
-        sort(events.begin(), events.end());
-        vector<int> mx(n,0);
-        mx[n-1] = events[n-1][2];
-        for(int i=n-2;i>=0;i--){
-            mx[i] = max(events[i][2], mx[i+1]);
-        }
-        int ans = 0;
+        vector<info> time(2*n);
         for(int i=0;i<n;i++){
-            int start = events[i][0];
-            int end = events[i][1];
-            int value = events[i][2];
-            vector<int> to_find = {end, INT_MAX, INT_MAX};
-            int indx = upper_bound(events.begin(), events.end(), to_find) - events.begin();
-                ans = max(ans, value+ (indx<n?mx[indx]:0));
-            
-            // cout<<ans<<" ";
+            int s = events[i][0];
+            int e = events[i][1];
+            int v = events[i][2];
+            time[2*i] = {s, 1, v};
+            time[2*i+1] = {e+1, 0, v};
+        }
+        sort(time.begin(), time.end());
+        int maxV = 0;
+        int ans = 0;
+        for(auto &[s, isStart, v] : time){
+            if(isStart) ans = max(ans, v+maxV);
+            else maxV = max(maxV, v);
         }
         return ans;
     }
